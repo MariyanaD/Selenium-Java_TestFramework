5dc8ea391d4dfc0d26d5133692c7fccd110c8d71
MariyanaD <mariyana.doichinova@gmail.com>
2023-01-10 11:51:43 +0200

    Add files via upload

--- src/main/java/utils/Browser.java
+++ src/main/java/utils/Browser.java
@@ -2,8 +2,9 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
 
-import java.util.concurrent.TimeUnit;
+
 
 public class Browser {
     public static WebDriver driver;
@@ -16,7 +17,7 @@ public class Browser {
                 "chromedriver_win32\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
     }
 
     /**
